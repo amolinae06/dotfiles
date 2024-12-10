c0157578651cad2f96a9ff13eddd8c88154cc29c
λmolinae <65742076+amolinae06@users.noreply.github.com>
2024-12-10 04:59:24 +0000

    added logout option

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # tofi powermenu
-case $(printf "%s\n" "Shut down" "Reboot" "Suspend" "Hibernate" | tofi -c ~/.config/tofi/configfs) in
-	"Shut down")
-		loginctl poweroff
-		;;
-	"Reboot")
-		loginctl reboot
-		;;
-	"Suspend")
-		loginctl suspend
-		;;
-	"Hibernate")
-		loginctl hibernate
-		;;
+case $(printf "%s\n" "Lock session" "Log out" "Shut down" "Reboot" "Suspend" | tofi -c ~/.config/tofi/configfs) in
+  "Log out")
+    hyprctl dispatch exit
+    ;;
+  "Suspend")
+    loginctl suspend
+    ;;
+  "Reboot")
+    loginctl reboot
+    ;;
+  "Shut down")
+    loginctl poweroff
 esac
+
